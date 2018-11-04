5a56316f36f9c9812ef5bfb5a33c0e038b502265
Anczimaru <anczi101@gmail.com>
2018-11-04 17:20:18 +0100

    0.01

--- Samolot.cpp
+++ Samolot.cpp
@@ -22,15 +22,19 @@ class Samolot
         double m_tech_state; //percentage of airplane maintenance
 
     public:
-        Samolot(int dest=1,int src=2,int capacity=80,int passengers=60,int status = 1,double fuel_state=0.8,double tech_state=0.8,int index=1):
+        /*Samolot(int dest=1,int src=2,int capacity=80,int passengers=60,int status = 1,double fuel_state=0.8,double tech_state=0.8,int index=1):
             m_dest(dest), m_src(src), m_capacity(capacity), m_passengers(passengers), m_status(status), m_fuel(fuel_state), m_tech_state(tech_state), m_index(index)
             {
                 cout<<"Im plane with no:"<<m_index<<endl;
                 fly_to_airport(m_dest);
-            }
+            } */
+        Samolot(int index=1):m_dest(1), m_src(2), m_capacity(80), m_passengers(60), m_status(0), m_fuel(0.78), m_tech_state(0.99), m_index(index)
+        {
+           // cout<<"   Im plane with no:"<<m_index<<endl;
+        }
         ~Samolot()
             {
-                //cout<<"destroyed plane"<<endl;
+                //cout<<"   destroyed plane no: "<<m_index<<endl;
             }
         void fly_to_airport(int airport_index)
             {
@@ -40,19 +44,25 @@ class Samolot
             {
                 return m_dest;
             }
-        void set_dest(int dest) 
-            {m_dest = dest;}
+        void set_dest(int target_dest) 
+            {m_dest = target_dest;}
         int get_plane_index()
-            {
-            return m_index;
-            }
+            { return m_index; }
         int get_capacity() 
             {return m_capacity;}
         int get_passengers()
             {return m_passengers;}
-        void set_passengers(int passengers)
-            {m_passengers = passengers;}
-
+        void set_passengers(int target_passengers)
+            {m_passengers = target_passengers;}
+        double get_fuel()
+            {return m_fuel;}
+        void set_fuel(double target_fuel)
+            {m_fuel = target_fuel;}
+        double get_tech_state()
+            {return m_tech_state;}
+        void set_tech_state(double target_state)
+            {m_tech_state = target_state;}
+    
     };
 
 class Airport
@@ -62,7 +72,7 @@ class Airport
         int m_waiting_ppl;
 
         vector <Samolot> samoloty;
-        Samolot m_plane;
+        Samolot m_tmp_plane; // to do change to pointer
 
     public:
         Airport(int index = 1) : m_index_ap(index)
@@ -81,33 +91,44 @@ class Airport
         
         void do_routine_on_plane()
             {
-                m_plane = samoloty.back();
-                loading();
+                m_tmp_plane = samoloty.back();
+                unload();
+                repair();
+                refuel();
+                load();
+                cout<<"\n" <<endl;
             }
         
-        double loading() //not sure bout type
+        double load() //not sure bout type
             {
-                int cap {m_plane.get_capacity()};
+                int cap {m_tmp_plane.get_capacity()};
                 int ppl_to_load;
                 (cap >= m_waiting_ppl)? (ppl_to_load = cap) : (ppl_to_load = m_waiting_ppl);
-                m_plane.set_passengers(ppl_to_load);
-                cout<<"Loaded : "<<m_plane.get_passengers()<<" passengers"<<endl;
+                m_tmp_plane.set_passengers(ppl_to_load);
+                cout<<"Loaded : "<<m_tmp_plane.get_passengers()<<" passengers"<<endl;
             }
-        double unload(int arr_passangers, int gate) //not sure bout type
+        double unload() //not sure bout type
             {
-                //unload passangers
+                int tmp_ppl_unloaded = m_tmp_plane.get_passengers();
+                m_tmp_plane.set_passengers(0);
+                cout<<"Unloaded: "<<tmp_ppl_unloaded<<" passengers"<<endl;
             }
-        double refuel(double m_fuel) //not sure bout type
+        double refuel() //not sure bout type
             {
-                //refuel airplane
+                m_tmp_plane.set_fuel(1.00);
+                cout<<"Refueled plane with index: "<<m_tmp_plane.get_plane_index()<<endl;
             }
-        int service(double m_tech_state)  //not sure bout type
+        int repair()  //not sure bout type
             {
-                // jeśli stan tech < 90 -> wyślij do naprawy
-            }
-        bool repair(double tech_state)
-            {
-                //napraw samolot
+                if (m_tmp_plane.get_tech_state() < 0.8)
+                    {
+                        m_tmp_plane.set_tech_state(1.00);
+                        cout<<"Repaired plane with index: "<<m_tmp_plane.get_plane_index()<<endl;
+                    }
+                else
+                    {
+                        cout<<"No maintenance needed for plane: "<<m_tmp_plane.get_plane_index()<<endl;
+                    }
             }
         int wait_in_hangar(int how_long, int how_long_left)
             {
@@ -131,10 +152,10 @@ class Airport
 int main(int argc, char* argv[])
 {
     Airport Los_Angles;
-    Samolot r1;
+    Samolot r1(1);
     Los_Angles.register_plane(r1);
     Los_Angles.do_routine_on_plane();
-    Samolot r2();
+    Samolot r2(2);
     Los_Angles.register_plane(r2);
     Los_Angles.do_routine_on_plane();
     //samoloty.back().introduce();
